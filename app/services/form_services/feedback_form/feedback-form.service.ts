ebe824fcabcae05aca660d7a5e316c45edfe841d
Francisco cruz <megafran@msn.com>
2023-07-10 22:35:14 -0500

    chore: update api url

--- app/services/form_services/feedback_form/feedback-form.service.ts
+++ app/services/form_services/feedback_form/feedback-form.service.ts
@@ -1,15 +1,12 @@
 import { SEND_FEEDBACK_EMAIL } from "../../api/variables"
 import { handleError, handleResponse } from "../../utils/reponse/response"
 
-async function sendFeedbackMessage(
-  formMessage: string,
-  isPositiveFeedback?: boolean,
-): Promise<Response> {
+async function sendFeedbackMessage(formMessage: string): Promise<Response> {
   return fetch(SEND_FEEDBACK_EMAIL, {
     method: "POST",
     mode: "cors",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ formMessage, isPositiveFeedback }),
+    body: JSON.stringify({ formMessage }),
   })
     .then(handleResponse)
     .catch(handleError)
